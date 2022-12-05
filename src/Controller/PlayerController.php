7cbcaa43fba99d3f622493df07ca43896e13d8eb
Lyxol <corbioli.lilian@gmail.com>
2022-12-05 09:55:54 +0100

    add routes playercontroller

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -15,4 +15,36 @@ class PlayerController extends AbstractController
             'controller_name' => 'PlayerController',
         ]);
     }
+
+    #[Route('/player/form')]
+    public function _form(): Response
+    {
+        return $this->render('player/_Form.html.twig',[
+            'controller_name' => 'PlayerController',
+        ]);
+    }
+
+    #[Route('/player/new')]
+    public function new(): Response
+    {
+        return $this->render('player/new.html.twig',[
+            'controller_name' => 'PlayerController',
+        ]);
+    }
+
+    #[Route('/player/edit')]
+    public function edit(): Response
+    {
+        return $this->render('player/edit.html.twig',[
+            'controller_name' => 'PlayerController',
+        ]);
+    }
+
+    #[Route('/player/delete')]
+    public function delete(): Response
+    {
+        return $this->render('player/delete.html.twig',[
+            'controller_name' => 'PlayerController',
+        ]);
+    }
 }
