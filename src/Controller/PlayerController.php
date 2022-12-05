0f031ab555c1c7a633265dd720dda400620181ad
Alexis BESSON <besson.alexis2@gmail.com>
2022-12-05 11:42:19 +0100

    resolution du bug

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -19,41 +19,18 @@ class PlayerController extends AbstractController
         ]);
     }
 
-    #[Route('/player/form')]
-    public function _form(Request $request): Response
+    #[Route('/player/new')]
+    public function new(Request $request): Response
     {
         $player = new Player();
         $form = $this->createForm(PlayerType::class, $player);
+        $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
             $player = $form->getData();
             return $this->redirectToRoute('/player');
         }
-        return $this->render('player/_Form.html.twig',[
-            'form' => $form
-        ]);
-    }
-
-    #[Route('/player/new')]
-    public function new(): Response
-    {
         return $this->render('player/new.html.twig',[
-            'controller_name' => 'PlayerController',
-        ]);
-    }
-
-    #[Route('/player/edit')]
-    public function edit(): Response
-    {
-        return $this->render('player/edit.html.twig',[
-            'controller_name' => 'PlayerController',
-        ]);
-    }
-
-    #[Route('/player/delete')]
-    public function delete(): Response
-    {
-        return $this->render('player/delete.html.twig',[
-            'controller_name' => 'PlayerController',
+            'form' => $form->createView()
         ]);
     }
 }
